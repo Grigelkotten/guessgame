1f6ec4a0da9faeaf0f2dd16603b538c05e51437e
Grigelkotten <alexander.asplen@gmail.com>
2024-10-03 22:35:43 +0200

    created glowing button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className="app-container">
-      <h2>Guesser Game</h2>
+      <h1>Welcome to Guesser Game</h1>
+      <h2>Are you ready to play?</h2>
       <Link to="/game">
-        <button>Start Game</button>
+        <button className="start-button">Start Game</button>
       </Link>
     </div>
   )
---
+++ src/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div>
+      <h1>Guesser Game</h1>
+      <p>Game content goes here</p>
+    </div>
+  );
+}
+
+export default Game;
